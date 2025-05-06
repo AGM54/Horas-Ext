0069830a7ca04d900290b150dc40b595dd52a7b6
Arthurs101 <arg21527@uvg.edu.gt>
2025-05-05 22:23:23 -0600

    fix: dashboard container

--- notes-management/app/modules/browse/components/DashboardContainer/styles.ts
+++ notes-management/app/modules/browse/components/DashboardContainer/styles.ts
@@ -3,5 +3,7 @@ import styled from '@emotion/styled'
 export const MainWrapper = styled.div(({theme}) => ({
     width: '100%',
     height: '100%',
-    backgroundColor: 'red'
+    backgroundColor: theme.colors.primaryDark,
+    display: 'flex',
+    flexDirection: 'column'
 }))
